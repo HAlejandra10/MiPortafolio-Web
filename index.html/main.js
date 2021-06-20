b504397e0d9cc9e78b771d2dfd7822b856ce0bfa
Alejandra Primo D <heidyprimo10@outlook.com>
2021-06-20 18:25:28 -0500

    anexado los archivos que se hicieron cambio

--- index.html/main.js
+++ index.html/main.js
@@ -2,7 +2,11 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector(".nav-menu").classList.toggle("show");
 });
 
+/* 80*/
 ScrollReveal().reveal('.showcase');
+/* 81*/
 ScrollReveal().reveal('.news-cards', { delay: 500 });
+/* 82*/
 ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+/* 83*/
 ScrollReveal().reveal('.cards-banner-two', { delay: 500 });;
